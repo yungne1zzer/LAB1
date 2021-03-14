ecc112832128f6cb90599702dcad11a667e316b9
yungne1zzer <yungne1zzer@gmail.com>
2021-03-14 11:27:09 +0100

    new 14.03

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -5,10 +5,11 @@ import pl.lublin.wsei.java.cwiczenia.mylib.Account;
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
-        Account acc = new Account();
-        acc.setName("piotr Gołabek");;
-        System.out.println(acc.getName());
+    public static void main(String []args){
+        System.out.printf("%s%n",Account.translit("Назар"));
     }
+
+
+
+
 }
--- src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
@@ -2,14 +2,46 @@ package pl.lublin.wsei.java.cwiczenia.mylib;
 import org.apache.commons.lang3.StringUtils;
 
 public class Account {
-    public String name;
-    public void setName(String name) {
-        String[] tokens = StringUtils.split(name);
+    private String name;
+
+    private static char ukrAlphabet[] = {'а', 'б', 'в', 'г', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р',
+            'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я'};
+
+    private static String translitRepl[] = {"a","b","v","g","d","e","je","zh","z","y","i","ji", "j",
+            "k","l","m","n","o","p","r","s","t","u","f","h","c",
+            "ch","sh","shh","'","ju","ja"};
+
+    public static String translit(String arg)
+    {
+        String arg1 = arg.toLowerCase();
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i < arg1.length(); i++)
+        {
+            for(int j = 0; j < ukrAlphabet.length; j++)
+            {
+                if (arg1.charAt(i) == ukrAlphabet[j])
+                {
+                    builder.append(translitRepl[j]);
+                }
+            }
+        }
+        return builder.toString();
+    }
+
+
+    public static String capitalize(String aStr)
+    {
+        String[] tokens = StringUtils.split(aStr);
         for(int i = 0; i < tokens.length; i++)
             tokens[i] = StringUtils.capitalize(tokens[i]);
-        this.name = StringUtils.join(tokens,' ');
+        return StringUtils.join(tokens,' ');
     }
+    public void setName(String name) {
+        this.name = capitalize(name);
+    }
+
     public String getName() {
         return name;
     }
+
 }
