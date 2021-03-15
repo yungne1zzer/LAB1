b847bfeafebfa38b165b258c272c84ecd123c4f6
yungne1zzer <yungne1zzer@gmail.com>
2021-03-15 14:13:42 +0100

    new 15.03

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,14 +2,48 @@ package pl.lublin.wsei.java.cwiczenia;
 
 import pl.lublin.wsei.java.cwiczenia.mylib.Account;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String []args){
-        System.out.printf("%s%n",Account.translit("янь"));
+        // 30 randomowych liczb
+//        int[] liczby = new int[30];
+//        Random rnd = new Random();
+//
+//        for(int i = 0; i<30; i++){
+//            liczby[i] = rnd.nextInt();
+//
+//        int mx = Integer.MIN_VALUE;
+//        int mn = Integer.MAX_VALUE;
+//        long avg = 0;
+//        for(int l :  liczby)
+//        {
+//            System.out.println(l);
+//            if(l<mn) mn = l;
+//            if(l>mx) mx = l;
+//            avg += l;
+//        }
+//            System.out.printf("MIN = %d, MAX =%d, AVG=%f,",mn,mx,(float)avg/liczby.length);
+
+//        zmiana rejestru
+//       Account acc= new Account();
+//       acc.setName("nazar Prosopchuk");
+//       acc.name = "nazar Prosopchuk";
+//       System.out.println(acc.getName());
+//       System.out.printf("%s%n",Account.capitalize("stanisŁaw maruSARz"));
+
+//      trasnlit
+
+       System.out.printf("%s%n",Account.translit("янь"));
+      }
+
     }
 
 
 
 
-}
+
+
+
--- src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
@@ -1,12 +1,15 @@
 package pl.lublin.wsei.java.cwiczenia.mylib;
+
 import org.apache.commons.lang3.StringUtils;
 
 public class Account {
-    private String name;
+    public String name;
 
-    private static char ukrAlphabet[] = {'а', 'б', 'в', 'г', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я'};
+    private static char ukrAlphabet[] = {'а', 'б', 'в', 'г', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о',
+            'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я'};
 
-    private static String translitPL[] = {"a","b","v","g","d","e","je","zh","z","y","i","ji", "j","k","l","m","n","o","p","r","s","t","u","f","h","c", "ch","sh","shh","'","ju","ja"};
+    private static String translitPL[] = {"a","b","v","g","d","e","je","zh","z","y","i","ji", "j","k","l","m","n","o",
+            "p","r","s","t","u","f","h","c", "ch","sh","shh","'","ju","ja"};
 
     public static String translit(String arg)
     {
@@ -26,19 +29,22 @@ public class Account {
     }
 
 
-    public static String capitalize(String aStr)
-    {
-        String[] tokens = StringUtils.split(aStr);
-        for(int i = 0; i < tokens.length; i++)
-            tokens[i] = StringUtils.capitalize(tokens[i]);
-        return StringUtils.join(tokens,' ');
-    }
-    public void setName(String name) {
-        this.name = capitalize(name);
-    }
-
-    public String getName() {
-        return name;
-    }
+//    public static String capitalize(String aStr)
+//    {
+//        String[] tokens = StringUtils.split(aStr);
+//        for(int i = 0; i < tokens.length; i++)
+//            tokens[i] = StringUtils.capitalize(tokens[i]);
+//        return StringUtils.join(tokens,' ');
+//    }
+//    public void setName(String name) {
+//        String[] tokens = StringUtils.split(name);
+//        for(int i = 0; i < tokens.length; i++)
+//            tokens[i] = StringUtils.capitalize(tokens[i]);
+//        this.name = StringUtils.join(tokens,' ');
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
 
 }
